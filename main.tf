1bf12a800db025d4a989ad28e41c4649ff67d481
wilsonoon2 <wilson.oontb@outlook.com>
2026-04-30 22:38:09 +0800

    Initial Terraform project

---
+++ main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+resource "aws_s3_bucket" "demo" {
+  bucket = "wilson-demo-bucket-12345"
+}
+
